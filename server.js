bad26bb72648e64f0433f1b8f30006876a58f1bf
TheIceHrt <125475770+TheIceHrt@users.noreply.github.com>
2025-02-26 11:40:20 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-const express = require("express");
+const net = require('net');
 
-const app = express();
-app.use(express.json());
+// Création du serveur TCP
+const server = net.createServer((socket) => {
+  console.log('Client connecté:', socket.remoteAddress + ':' + socket.remotePort);
 
-app.post("/receiveData", (req, res) =>{
-    console.log("Données reçues :",
-req.body);
-    res.status(200).send("Donnée reçue !"); 
+  // Lorsque le serveur reçoit des données du client
+  socket.on('data', (data) => {
+    console.log('Données reçues:', data.toString());
+    // Envoie d'une réponse simple au client
+    socket.write('Donnée reçue !');
+  });
+
+  // Gestion de la déconnexion du client
+  socket.on('end', () => {
+    console.log('Client déconnecté');
+  });
+
+  // Gestion des erreurs sur le socket
+  socket.on('error', (err) => {
+    console.error('Erreur sur le socket:', err);
+  });
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Serveur en écoute sur le port ${PORT}`)
-);
 
-
-
-
+// Le port est défini par la variable d'environnement (utile pour certains hébergeurs) ou par défaut 12345
+const PORT = process.env.PORT || 12345;
+server.listen(PORT, () => {
+  console.log('Serveur TCP en écoute sur le port', PORT);
+});
